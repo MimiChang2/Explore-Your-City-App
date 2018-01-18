c55ade0fb983aa4cd55e341ec9eccf7c77e87809
Mimi Chang <mimichang94@yahoo.com>
2018-01-18 23:14:50 +0000

    Event and User Model and Controller Done

---
+++ controllers/EventController.js
@@ -0,0 +1,62 @@
+//require the models folder
+var db = require("../models");
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+app.use(router);
+
+//"get" route for all current events...
+router.get("/api/events", function(req, res) {
+    db.Event.findAll({ include: [db.User] }).then(function(allEvents) {
+        res.json(allEvents);
+    });
+});
+
+//"get" route for one specific event by ID
+router.get("/api/events/:id", function(req, res) {
+    db.Event.findOne({
+        id: req.params.id
+    }).then(function(data) {
+        res.json(data);
+    });
+});
+
+//post route for users to post a new event... 
+router.post("/api/events", function(req, res) {
+    console.log(req.body);
+    //create a new event and pass it into an object
+    db.Event.create({
+        eventname: req.body.eventname,
+        location: req.body.location,
+        date: req.body.date,
+        description: req.body.description
+    }).then(function(newEvent) {
+        res.json(newEvent);
+    });
+});
+
+//delete route to delete their posting. For someone who has posted an item only. 
+router.delete("/api/events/:id", function(req, res) {
+    db.Event.destroy({
+        id: req.params.id
+    }).then(function(data) {
+        res.json(data);
+    });
+});
+
+//PUT route for updating postings. For someone who has posted an item only. 
+router.put("/api/events", function(req, res) {
+    db.Event.update({
+        eventname: req.body.eventname,
+        location: req.body.location,
+        date: req.body.date,
+        description: req.body.description
+    }, {
+        where: { id: req.body.id }
+    }).then(function(data) {
+        res.json(data);
+    });
+});
+
+module.exports = router;
---
+++ models/EventModel.js
@@ -0,0 +1,64 @@
+//this is a table. var Events is the name of the table
+
+//association joins (belongsTo and HasMany) go in the model 
+
+module.exports = function(sequelize, DataTypes) {
+
+    var Event = sequelize.define("Event", {
+        eventName: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                len: [10, 40]
+            }
+        },
+        location: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            validate: {
+                len: [6, 40]
+            }
+        },
+        date: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            validate: {
+                len: [10, 40]
+            }
+        },
+        description: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            validate: {
+                len: [10, 40]
+            }
+        },
+    });
+
+    Event.associate = function(models) {
+        // This will add the user id to the Event
+        // The foreign key added to the Event is the user id.
+
+        Event.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        // This will add the Event id to the RSVP and Comments
+        // Each event can have many Comments and RSVPs
+        // The foreign key added to the Comment and RSVP is the Event id.
+        Event.hasMany(models.Comment, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+        
+        Event.hasMany(models.RSVP, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Event;
+};
